1c13f9242c827412e26840948544baee016125ff
zhu-ty <zhu-ty@outlook.com>
2019-01-21 14:58:16 +0800

    added support for ABGR & ARGB h265 encoding

--- main.cpp
+++ main.cpp
@@ -13,20 +13,19 @@
 
 #include "SKEncoder.h"
 
-#define FRAME_NUM 300
+#define FRAME_NUM 100
+//#define TEST_YUV
+#define TEST_ABGR
+//#define TEST_ARGB
 
 int main(int argc, char* argv[]) 
 {
-	//cv::VideoCapture at("t.mp4");
-	//cv::Mat tt;
-	//int cc = at.get(cv::CAP_PROP_FRAME_WIDTH);
-	//at >> tt;
-
-
-
-	SKEncoder encoder;
-	//const int frameNum = 100;
 	const int w = 4112, h = 3008;
+	SKEncoder encoder;
+	
+
+#ifdef TEST_YUV
+	encoder.init(FRAME_NUM, cv::Size(w, h));
 	cv::cuda::GpuMat gm[FRAME_NUM][3];
 	for (int i = 0; i < FRAME_NUM; i++)
 	{
@@ -34,29 +33,22 @@ int main(int argc, char* argv[])
 		cv::Mat YUV;
 		cv::cvtColor(im, YUV, cv::COLOR_BGR2YUV_I420);
 		//cv::cvtColor(im, YUV, cv::COLOR_BGR2YUV);
-
 		cv::Mat Y = YUV(cv::Rect(0, 0, im.cols, im.rows));
 		cv::Mat U = YUV(cv::Rect(0, im.rows, im.cols, im.rows / 4));
 		U = U.reshape(1, im.rows / 2);
 		cv::Mat V = YUV(cv::Rect(0, im.rows + im.rows / 4, im.cols, im.rows / 4));
 		V = V.reshape(1, im.rows / 2);
-
 		gm[i][0].upload(Y);
 		gm[i][1].upload(U);
 		gm[i][2].upload(V);
-
 		if(i % 20 == 0)
 			SysUtil::infoOutput(SysUtil::format("Buffering Data Frame = %d, All = %d", i, FRAME_NUM));
-
 	}
 	SysUtil::infoOutput("Data All Buffered, Start Encoding.");
-
-	encoder.init(FRAME_NUM, cv::Size(w, h));
 	for (int i = 0; i < FRAME_NUM; i++)
 	{
 		std::vector<void*> YUV_pointer(3);
 		std::vector<uint32_t> steps(3);
-
 		for (int j = 0; j < 3; j++)
 		{
 			YUV_pointer[j] = gm[i][j].data;
@@ -64,9 +56,25 @@ int main(int argc, char* argv[])
 		}
 		encoder.encode(YUV_pointer, steps);
 	}
+#elif defined(TEST_ABGR)
+	encoder.init(FRAME_NUM, cv::Size(w, h),"outputABGR.h265", SKEncoder::FrameType::ABGR);
+	cv::cuda::GpuMat gm[FRAME_NUM];
+	for (int i = 0; i < FRAME_NUM; i++)
+	{
+		cv::Mat im = cv::imread(SysUtil::format("data/CUCAU1731016_00_%05d.jpg", i));
+		cv::Mat ABGR;
+		cv::cvtColor(im, ABGR, cv::COLOR_BGR2RGBA);
+		gm[i].upload(ABGR);
+		if (i % 20 == 0)
+			SysUtil::infoOutput(SysUtil::format("Buffering Data Frame = %d, All = %d", i, FRAME_NUM));
+	}
+	SysUtil::infoOutput("Data All Buffered, Start Encoding.");
+	for (int i = 0; i < FRAME_NUM; i++)
+	{
+		encoder.encode(gm[i].data, gm[i].step);
+	}
+#endif
 	encoder.endEncode();
-
 	getchar();
-
 	return 0;
 }
